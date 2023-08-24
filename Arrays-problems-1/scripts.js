3b52a32ffb237f8c29b95aca0f932c575b6dafde
Larry-cool <larrycoolboy92@gmail.com>
2023-08-24 16:24:37 +0200

    added comments to my codes/wrote the function for problem 2

--- Arrays-problems-1/scripts.js
+++ Arrays-problems-1/scripts.js
@@ -4,17 +4,21 @@ an array and a number (n), and return the first n elements of that array.
 (Example: If I pass in an array and the number 2, 
 your function returns the first 2 elements in that array.)
 */
-
+// declare my array elements of fruits.
 const fruitsArray = ["Banana", "Apple", "Kiwi", "Orange", "Lemon"];
 
-const getArrItems = function (array, num) {
+//function that takes in two parameters, an array and a number (n).
 
+const getArrItems = function (array, num) {
+//using the === to make sure the types and values entered are true.
 	if (array === null || num === null) 
 		return "Your input should be ('array','number'), please try again.";
 
+//check if the number inputed is greater than 0;
 	if (num < 0)
 		return "Please enter a positive value for num";
 
+//check if the number inputed is greater than the array length;
 	if(num > array.length) 
 		return "We only have " + array.length + " items. Please try again.";
 	
---
+++ arrays-problems-2/script.js
@@ -0,0 +1,18 @@
+/*
+Problem 2: Write a simple JavaScript program to 
+join all elements of the following array into a string. 
+using the array join() method
+*/
+
+//declare my arrays of cars
+const cars = ["Toyota", "Jeep", "Honda", "Volvo"];
+
+//create a variable to store the elements after using the join() method.
+// the default separator for join() is Comma. 
+let text = cars.join();
+
+// declare my function.
+function joinElement(cars) {
+	return text;
+
+}
