29fdb79dc6630a8e191a9d810f77f0e1ee250207
Larry-cool <larrycoolboy92@gmail.com>
2023-08-24 18:05:25 +0200

    wrote the Js function and added comments to my code

---
+++ arrays-problems-3/script.js
@@ -0,0 +1,16 @@
+/*Define a function that takes in an array of numbers/integers and returns the sorted 
+version of that array, greatest to least. (Note: do NOT use the "reverse" method.)
+*/
+
+const myNumArr = [3,4,-2,13,8,43,-5,22,100];
+// using the Array sort() method and comparing function to sort it 
+// from greatest to lowest.
+
+ let list = myNumArr.sort((a,b) => b - a );
+
+//console.log(myNumArr);
+
+ function sortedArr(myNumArr) {
+ 	return list;
+
+ }
